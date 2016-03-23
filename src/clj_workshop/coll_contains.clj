00c9180e323bf42e3ce14b0234221f4d8a830430
krzychukula <krzychukula@gmail.com>
2016-03-23 12:21:41 +0100

    Solve exercises

--- src/clj_workshop/coll_contains.clj
+++ src/clj_workshop/coll_contains.clj
@@ -3,4 +3,9 @@
 (defn coll-contains?
   "Returns true if the provided coll contains the given x value"
   [coll x]
-  (throw (ex-info "Unimplemented" {})))
+  (if (empty? coll) false
+    (let [y (first coll)
+          tail (rest coll)]
+      (or (= x y) (coll-contains? tail x)))))
+
+
--- src/clj_workshop/parens_validator.clj
+++ src/clj_workshop/parens_validator.clj
@@ -1,8 +1,23 @@
 (ns clj-workshop.parens-validator)
 
+(defn count-parens
+  "Return the number of parens in the vector"
+  [par]
+  (if (empty? par) 0
+    (let [x (first par)
+          tail (rest par)
+          num-value (cond
+                      (= x \() 1
+                      (= x \)) -1
+                      :otherwise 0)]
+      (+ num-value (count-parens tail)))))
+
 (defn parens-balanced?
   "Returns true if parens in the provided string
   are balanced, false otherwise. Ignores any other
   characters."
   [s]
-  (throw (ex-info "Unimplemented" {})))
+  (let [num-of-parens (count-parens (vec s))]
+    (cond
+      (zero? num-of-parens) true
+      :otherwise false)))
