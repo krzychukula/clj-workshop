101bab4db2cbe49a4ff85eeb9ba0cd4dc16d7164
krzychukula <krzychukula@gmail.com>
2016-03-23 12:50:17 +0100

    fix )( parens case

--- src/clj_workshop/parens_validator.clj
+++ src/clj_workshop/parens_validator.clj
@@ -2,22 +2,24 @@
 
 (defn count-parens
   "Return the number of parens in the vector"
-  [par]
-  (if (empty? par) 0
+  [depth par]
+  (if (empty? par) depth
     (let [x (first par)
           tail (rest par)
           num-value (cond
                       (= x \() 1
                       (= x \)) -1
-                      :otherwise 0)]
-      (+ num-value (count-parens tail)))))
+                      :otherwise 0)
+          new-depth (+ depth num-value)]
+      (if (> 0 new-depth) new-depth
+        (count-parens new-depth tail)))))
 
 (defn parens-balanced?
   "Returns true if parens in the provided string
   are balanced, false otherwise. Ignores any other
   characters."
   [s]
-  (let [num-of-parens (count-parens (vec s))]
+  (let [num-of-parens (count-parens 0 (vec s))]
     (cond
       (zero? num-of-parens) true
       :otherwise false)))
